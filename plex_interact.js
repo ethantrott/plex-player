02d577cf98cc0a4ef38ca230a5f480c66886c644
Ethan Trott <me@ethantrott.com>
2023-04-23 15:02:16 -0400

    update interaction IDs

--- plex_interact.js
+++ plex_interact.js
@@ -38,10 +38,12 @@ async function loginExternal(returnURL){
 
     console.log("Successfully logged in to Plex");
 
+    //wait a second to allow page to load
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
     //we can attempt to play the video again now
     await driver.get(returnURL);
 }
-
 
 async function playVideo(videoURL) {
     //load the crunchyroll page
@@ -63,39 +65,31 @@ async function playVideo(videoURL) {
     }
     else console.log("Already signed in, looks good to go");
 
-    //wait for player to load
-    await playerIsDoneLoading();
-
-    //have to switch to video player frame. why? idk, selenium.
-    await driver.switchTo().frame(driver.findElement(By.className("video-player")));
-
-    //click play button if present
-    try{
-        let playButton = await driver.findElement(By.css("[data-testid='vilos-large_play_pause_button']"));
-        console.log("Found play button, clicking..");
-        await playButton.click();
-    } catch(e) {
-        console.log("No play button present, probably autoplayed");
-    }
+    //click shuffle button
+    await driver.wait(until.elementLocated(By.css("[data-testid='toolbarShuffle")), 15 * 1000);
+    await driver.findElement(By.css("[data-testid='toolbarShuffle']")).click();
 
     //click the fullscreen button
-    await driver.wait(until.elementLocated(By.css("[data-testid='vilos-fullscreen_button']")), 15 * 1000);
-    await driver.findElement(By.css("[data-testid='vilos-fullscreen_button']")).click();
+    await driver.wait(until.elementLocated(By.css("[data-testid='fullscreenButton']")), 15 * 1000);
+    await driver.findElement(By.css("[data-testid='fullscreenButton']")).click();
     console.log("Entered fullscreen");
     
     isPaused = false;
-
-    return new Promise((resolve) => {
-        resolve(videoIsFinished());
-    });
 };
 
 async function clickPauseToggle(){
     await moveMouse();
 
-    //click the play/pause button
-    await driver.wait(until.elementLocated(By.css("[data-testid='vilos-play_pause_button']")), 15 * 1000);
-    await driver.findElement(By.css("[data-testid='vilos-play_pause_button']")).click();
+    if (!isPaused){
+        //click the pause button
+        await driver.wait(until.elementLocated(By.css("[data-testid='pauseButton']")), 15 * 1000);
+        await driver.findElement(By.css("[data-testid='pauseButton']")).click();
+    }
+    else{
+         //click the pause button
+         await driver.wait(until.elementLocated(By.css("[data-testid='resumeButton']")), 15 * 1000);
+         await driver.findElement(By.css("[data-testid='resumeButton']")).click();
+    }
 
     resetMouse();
 
@@ -117,13 +111,29 @@ async function skipVideo(){
     await moveMouse();
 
     //click the skip button
-    await driver.wait(until.elementLocated(By.css("[data-testid='vilos-next_episode_button']")), 15 * 1000);
-    await driver.findElement(By.css("[data-testid='vilos-next_episode_button']")).click();
+    await driver.wait(until.elementLocated(By.css("[data-testid='nextButton']")), 15 * 1000);
+    await driver.findElement(By.css("[data-testid='nextButton']")).click();
 
     resetMouse();
 
     isPaused = false;
     console.log("Skipped episode");
+}
+
+async function moveMouse(){
+    driver.actions().move({
+        origin: 'pointer',
+        x: 20,
+        y: 20
+    }).perform();
+}
+
+async function resetMouse(){
+    driver.actions().move({
+        origin: 'viewport',
+        x: 20,
+        y: 20
+    }).perform();
 }
 
 module.exports.playVideo = playVideo;
