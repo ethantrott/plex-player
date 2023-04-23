ac71277e62df5dcb3c5bf18a14d3fc740deb6542
Ethan Trott <me@ethantrott.com>
2023-04-23 16:57:55 -0400

    name correction

--- control_api.js
+++ control_api.js
@@ -6,38 +6,38 @@ var express = require('express');
 var app = express();
 let exec = require('child_process').exec;
 
-var cr_interact = require("./plex_interact");
+var plex_interact = require("./plex_interact");
 
 app.get('/displayon', function (req, res) {
     res.send("ok");
     console.log('Received command to turn display on.');
     exec('xset -display :0.0 dpms force on');
-    cr_interact.resumeVideo();
+    plex_interact.resumeVideo();
 });
 
 app.get('/displayoff', function (req, res) {
     res.send("ok");
     console.log('Received command to turn display off.');
     exec('xset -display :0.0 dpms force off');
-    cr_interact.pauseVideo();
+    plex_interact.pauseVideo();
 });
 
 app.get('/pause', function (req, res) {
     res.send("ok");
     console.log('Received command to pause playback.');
-    cr_interact.pauseVideo();
+    plex_interact.pauseVideo();
 });
 
 app.get('/resume', function (req, res) {
     res.send("ok");
     console.log('Received command to resume playback.');
-    cr_interact.resumeVideo();
+    plex_interact.resumeVideo();
 });
 
 app.get('/skip', function (req, res) {
     res.send("ok");
     console.log('Received command to skip episode playback.');
-    cr_interact.skipVideo();
+    plex_interact.skipVideo();
 });
 
 app.get('/test', function (req, res) {
